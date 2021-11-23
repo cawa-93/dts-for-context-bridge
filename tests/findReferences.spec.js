fab78493128bafb263a142839647c70557981beb
Alex Kozack <cawa-93@users.noreply.github.com>
2021-11-23 13:45:58 +0200

    test: Add simple tests

---
+++ tests/findReferences.spec.js
@@ -0,0 +1,68 @@
+import {test} from 'uvu';
+import * as assert from 'uvu/assert';
+import {findReferences} from "../findReferences.js";
+import {Project} from "ts-morph";
+
+test('default import', () => {
+
+    const expectedKey = '"EXPECTED_KEY"'
+    const expectedType = '"EXPECTED_TYPE"'
+
+    const project = new Project()
+    project.createSourceFile(
+        './tmp.ts',
+        'import electron from "electron"'
+        + `electron.contextBridge.exposeInMainWorld(${expectedKey}, ${expectedType})`
+    )
+    const references = findReferences(project)
+    assert.is(references.get(expectedKey), expectedType)
+})
+
+
+test('default import with alias', () => {
+
+    const expectedKey = '"EXPECTED_KEY"'
+    const expectedType = '"EXPECTED_TYPE"'
+
+    const project = new Project()
+    project.createSourceFile(
+        './tmp.ts',
+        'import electronAlias from "electron"'
+        + `electronAlias.contextBridge.exposeInMainWorld(${expectedKey}, ${expectedType})`
+    )
+    const references = findReferences(project)
+    assert.is(references.get(expectedKey), expectedType)
+})
+
+
+test('named import', () => {
+
+    const expectedKey = '"EXPECTED_KEY"'
+    const expectedType = '"EXPECTED_TYPE"'
+
+    const project = new Project()
+    project.createSourceFile(
+        './tmp.ts',
+        'import {contextBridge} from "electron"'
+        + `contextBridge.exposeInMainWorld(${expectedKey}, ${expectedType})`
+    )
+    const references = findReferences(project)
+    assert.is(references.get(expectedKey), expectedType)
+})
+
+test('named import with alias', () => {
+
+    const expectedKey = '"EXPECTED_KEY"'
+    const expectedType = '"EXPECTED_TYPE"'
+
+    const project = new Project()
+    project.createSourceFile(
+        './tmp.ts',
+        'import {contextBridge as contextBridgeAlias} from "electron"'
+        + `contextBridgeAlias.exposeInMainWorld(${expectedKey}, ${expectedType})`
+    )
+    const references = findReferences(project)
+    assert.is(references.get(expectedKey), expectedType)
+})
+
+test.run()
