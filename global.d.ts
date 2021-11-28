21a80c73a24117351c77478160e9dc08fde429b1
Alex Kozack <cawa-93@users.noreply.github.com>
2021-11-28 15:01:16 +0200

    chore: Mute TS warnings

--- global.d.ts
+++ global.d.ts
@@ -24,5 +24,17 @@ declare module 'electron' {
     export const ipcRenderer: IpcRenderer
     export const shell: Shell
     export const contextBridge: ContextBridge
+}
 
+/**
+ * Needed to mute annoying warnings from the TS
+ * @example
+ *
+ * // TS2550: Property 'startsWith' does not exist on type 'string'.
+ * // Do you need to change your target library?
+ * // Try changing the 'lib' compiler option to 'es2015' or later.
+ * url.startsWith('http')
+ */
+interface String {
+    startsWith: (s: string) => boolean
 }
